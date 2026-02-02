65a5a7a23d0b5ab6fd5e508cecab08149457968d
mattg77-eng <mattg77@my.yorku.ca>
2026-02-02 12:43:03 -0500

    Delete Lab2 directory

--- Lab2/Lab2_3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "$1 "
-
-echo -n "- "
-
-shift 3
-echo "$@"
-
--- Lab2/lab2_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-day_of_the_week=$(date +%A)
-
-user=$(whoami)
-
-echo "Good day, Mr/Ms $user! Today is $day_of_the_week."
-
